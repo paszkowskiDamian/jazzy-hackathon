9ad983b9a06d227557ea438a5fb5bc9ae18b0236
Artur Kasperek <arturmiks@gmail.com>
2017-05-26 22:23:01 +0200

    Added mocked routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -22,14 +22,154 @@ exports = module.exports = function (app) {
 	// Views
 	app.get('/', routes.views.index);
 
-	app.get('/test', (req, res) => {
-		res.send('tested route ;)');
+	app.get('/users/:id', ({params}, res, next) => {
+		const { id } = params;
+
+
+		res.json({
+			name: 'Tomek Ferens',
+			id: id,
+			avatar: 'https://www.contentchampion.com/wp-content/uploads/2013/11/avatar-placeholder.png',
+			projects: [0, 1, 2, 5],
+			organizations: [3, 4, 6],
+			socialMedia: {
+				fb: 'https://expressjs.com/en/starter/installing.html',
+				github: 'https://expressjs.com/en/starter/installing.html',
+				linkedin: 'https://expressjs.com/en/starter/installing.html',
+			},
+			about: 'Czesc mam na imie tomek, lubię jeść',
+			skills: ['js', 'php', 'good practice', 'architecture'],
+			interests: ['informatyka', 'dziewczyny', 'jedzenie', 'machine learnign']
+		});
 	});
 
-	app.get('/tested-json', (req, res) => {
+	app.get('/organizations/:id', ({params}, res, next) => {
+		const { id } = params;
+
 		res.json({
-			a: 'some',
-			b: 'value',
+			name: 'Klub jedzenia',
+			id,
+			projects: [1, 2],
+			description: '<h1>Klub jedzenia wita!</h1><br/><span>Jestesmy klubem jedzenia xD</span>>',
+			logo: 'https://image.shutterstock.com/display_pic_with_logo/698308/698308,1316410880,14/stock-photo-supreme-pizza-lifted-slice-84904912.jpg',
+			location: {
+				long: 50.2899672,
+				lat: 18.6764128,
+			},
+			events: [],
+			admins: [1, 3, 5],
+			socialMedia: {
+				fb: 'https://expressjs.com/en/starter/installing.html',
+				github: 'https://expressjs.com/en/starter/installing.html',
+				linkedin: 'https://expressjs.com/en/starter/installing.html',
+			},
+			periodicMeetup: null,
+			tags: ['informatyka', 'jedzenie', 'dupy'],
+
+		});
+	});
+
+
+	app.get('/project/:id', ({params}, res, rext) => {
+		const { id } = params;
+
+		res.json({
+			name: 'Zjedz pizze chellange',
+			id,
+			ownerId: 50,
+			users: [0, 1, 5],
+			posts: [{
+				title: 'Jemy',
+				image: 'https://image.shutterstock.com/display_pic_with_logo/698308/698308,1316410880,14/stock-photo-supreme-pizza-lifted-slice-84904912.jpg',
+				body: 'Zjedz chellage opis'
+			}, {
+				title: 'Jemy 2',
+				image: 'https://image.shutterstock.com/display_pic_with_logo/698308/698308,1316410880,14/stock-photo-supreme-pizza-lifted-slice-84904912.jpg',
+				body: 'Zjedz chellage 2 opis'
+			}, {
+				title: 'Jemy 3',
+				image: 'https://image.shutterstock.com/display_pic_with_logo/698308/698308,1316410880,14/stock-photo-supreme-pizza-lifted-slice-84904912.jpg',
+				body: 'Zjedz chellage 3 opis'
+			}],
+			image: 'https://image.shutterstock.com/display_pic_with_logo/698308/698308,1316410880,14/stock-photo-supreme-pizza-lifted-slice-84904912.jpg',
+			description: '<h1>Zledz pizza projekt!</h1><br/><span>O tym projekcie</span>',
+			socialMedia: {
+				fb: 'https://expressjs.com/en/starter/installing.html',
+				github: 'https://expressjs.com/en/starter/installing.html',
+				linkedin: 'https://expressjs.com/en/starter/installing.html',
+			},
+			tags: ['jedzenie', 'pizza', 'dupy'],
+			skillsNeeded: ['eating', 'fat', 'js'],
+			likes: 5,
+		});
+	});
+
+	app.get('/events/:id', ({params}, res, rext) => {
+		const { id } = params;
+
+		res.json({
+			name: 'Robienie pizzy',
+			id,
+			location: {
+				long: 50.2899672,
+				lat: 18.6764128,
+			},
+			users: [0, 34, 54, 2],
+			description: '<h1>Robienie pizzy!</h1><br/><span>O tym projekcie robieniu pizzy</span>',
+			ownerId: 6,
+			tags: ['jedzenie', 'js', 'informatyka'],
+		});
+	});
+
+	app.get('/tags', ({params}, res, rext) => {
+		res.json({
+			tags: ['jedzenie', 'js', 'informatyka', 'pizza', 'dupy']
+		});
+	});
+
+	app.get('/skills', ({params}, res, rext) => {
+		res.json({
+			skills: ['js', 'php', 'good practice', 'architecture'],
+		});
+	});
+
+	app.get('/search/:name', ({params}, res, rext) => {
+		res.json({
+			organizations: [{
+				name: 'Klub jedzenie',
+				id: 43,
+				logo: 'https://image.shutterstock.com/display_pic_with_logo/698308/698308,1316410880,14/stock-photo-supreme-pizza-lifted-slice-84904912.jpg'
+			}, {
+				name: 'Klub jedzenia 2',
+				id: 40,
+				logo: 'https://image.shutterstock.com/display_pic_with_logo/698308/698308,1316410880,14/stock-photo-supreme-pizza-lifted-slice-84904912.jpg'
+			}, {
+				name: 'Klub jedzenie 3',
+				id: 1,
+				logo: 'https://image.shutterstock.com/display_pic_with_logo/698308/698308,1316410880,14/stock-photo-supreme-pizza-lifted-slice-84904912.jpg'
+			}],
+			projects: [{
+				name: 'Projekt 1',
+				id: 20,
+				logo: 'https://image.shutterstock.com/display_pic_with_logo/698308/698308,1316410880,14/stock-photo-supreme-pizza-lifted-slice-84904912.jpg'
+			}, {
+				name: 'Projekt 2',
+				id: 3,
+				logo: 'https://image.shutterstock.com/display_pic_with_logo/698308/698308,1316410880,14/stock-photo-supreme-pizza-lifted-slice-84904912.jpg'
+			}, {
+				name: 'Projekt 3',
+				id: 1,
+				logo: 'https://image.shutterstock.com/display_pic_with_logo/698308/698308,1316410880,14/stock-photo-supreme-pizza-lifted-slice-84904912.jpg'
+			}],
+			events: [{
+				name: 'Event 1',
+				id: 30,
+				logo: 'https://image.shutterstock.com/display_pic_with_logo/698308/698308,1316410880,14/stock-photo-supreme-pizza-lifted-slice-84904912.jpg'
+			}, {
+				name: 'Event 2',
+				id: 220,
+				logo: 'https://image.shutterstock.com/display_pic_with_logo/698308/698308,1316410880,14/stock-photo-supreme-pizza-lifted-slice-84904912.jpg'
+			}]
 		})
-	})
+	});
 };
