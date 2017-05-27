b20cbfd4aaf702426e85781a9cd5d4189ec6581b
DamianSky <paszko.damian@gmail.com>
2017-05-27 15:26:40 +0200

    show/hide modal

--- frontend/src/views/components/RegistrationModal.js
+++ frontend/src/views/components/RegistrationModal.js
@@ -3,14 +3,10 @@ import cn from 'classnames';
 
 
 export class RegistrationModal extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
-        console.log(this.props.hide);
+        const {isHidden, toggleModal } = this.props;
         return (
-            <div className={cn('registration-modal', { show: this.props.hide })} onClick={} >
+            <div className={cn('registration-modal', { hide: isHidden })} onClick={ () => toggleModal()  } >
                 MODAL
             </div>
         );
