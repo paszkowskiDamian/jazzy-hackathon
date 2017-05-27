4d6c8a1149464e3786f69d1f0886fd61d13825fe
Artur Kasperek <arturmiks@gmail.com>
2017-05-28 00:26:14 +0200

    Added pinned header

--- frontend/src/views/Navigation/Navigation.js
+++ frontend/src/views/Navigation/Navigation.js
@@ -4,13 +4,42 @@ import { Logo } from '../components/Logo';
 import { Tabs } from '../components/Tabs';
 import { SearchInput } from '../components/SearchInput';
 import { UserTab } from '../components/UserTab';
+import classnames from 'classnames';
 
 export class Navigation extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			scrollDirectionDown: false,
+			scrollTopPosition: 0,
+			pinned: true,
+		};
+	}
+
+	componentWillMount() {
+		const mainContainer = document.querySelector('#root');
+		const viewContent = document.querySelector('#root');
+		let scrolling = false;
+
+		window.addEventListener('scroll', () => {
+				const oldScrollTopPosition = this.state.scrollTopPosition;
+				const currentScrollTopPosition = viewContent.getBoundingClientRect().top;
+				const scrollDirectionDown = currentScrollTopPosition - oldScrollTopPosition < 0;
+
+				this.setState({
+					...this.state,
+					scrollDirectionDown,
+					scrollTopPosition: currentScrollTopPosition,
+				});
+		});
+	}
+
     render() {
         const { name, avatar } = this.props.loggedInUser;
 
         return (
-            <div className="navigation">
+            <div className={classnames('navigation', {'hide-nav': this.state.scrollDirectionDown})}>
                 <Logo />
                 <Tabs />
                 <SearchInput />
