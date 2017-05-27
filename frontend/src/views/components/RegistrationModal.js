6e36063016cfeea7d543b3483d2b43bfa3ff091a
DamianSky <paszko.damian@gmail.com>
2017-05-27 16:56:00 +0200

    modal

--- frontend/src/views/components/RegistrationModal.js
+++ frontend/src/views/components/RegistrationModal.js
@@ -4,10 +4,13 @@ import cn from 'classnames';
 
 export class RegistrationModal extends Component {
     render() {
-        const {isHidden, toggleModal } = this.props;
+        const { isHidden, toggleModal } = this.props;
         return (
-            <div className={cn('registration-modal', { hide: isHidden })} onClick={ () => toggleModal()  } >
-                MODAL
+            <div className="modal">
+                <div className={cn('modal-card-container', { swipe: isHidden })} > 
+                    <div className={'modal-card'}>CARD</div>
+                </div>
+                <div className={cn('bg-modal', { hide: isHidden })} onClick={() => toggleModal()} ></div>
             </div>
         );
     }
