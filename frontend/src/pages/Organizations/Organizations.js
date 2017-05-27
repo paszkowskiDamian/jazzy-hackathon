605c9bf7907e24ef59563b409a54c3efc18be2ed
DamianSky <paszko.damian@gmail.com>
2017-05-27 02:53:24 +0200

    geolocation

--- frontend/src/pages/Organizations/Organizations.js
+++ frontend/src/pages/Organizations/Organizations.js
@@ -5,9 +5,7 @@ import { Map } from './../../views/components/Map'
 export class Organizations extends Component {
     render() {
         return (
-            <div style={{ width: '100%', height: '60vh' }}>
                 <Map />
-            </div>
         );
     }
 }
